78ab7cc4e8ad30361b87377a8ad62f77f8dec0d3
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-03-19 00:18:55 +0100

    [FEATURE] release 6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,15 +3,21 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Allows to see FE only for logged BE users.',
     'description' => 'When you have stages like LOCAL/DEV/LIVE you may want DEV to be access restricted. One way is to generate htaccess for that. The second is to use this extension which allows only logged BE user to see FE.',
-    'category' => 'fe',
-    'version' => '5.0.0',
-    'state' => 'stable',
-    'uploadfolder' => false,
-    'createDirs' => '',
-    'clearcacheonload' => 0,
     'author' => 'SourceBroker Team',
     'author_email' => 'office@sourcebroker.net',
     'author_company' => 'SourceBroker',
+    'category' => 'fe',
+    'version' => '6.0.0',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'lockType' => '',
+    'clearCacheOnLoad' => 0,
     'constraints' => [
         'depends' => [
             'typo3' => '6.2.0-8.6.99',
@@ -19,4 +25,9 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [],
         'suggests' => [],
     ],
+    'autoload' => [
+        'psr-4' => [
+            'SourceBroker\\Restrictfe\\' => 'Classes',
+        ]
+    ]
 ];
