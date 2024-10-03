9966f4af94d67aeb00cc67babefa93c0cd72f54d
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2024-10-03 15:04:05 +0200

    [TASK] Refresh ddev config, reformat sql file. Remove scrutinizer check.

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,4 @@
-
-CREATE TABLE be_users (
-  tx_restrictfe_clearbesession tinyint(4) DEFAULT '0' NOT NULL,
+CREATE TABLE be_users
+(
+    tx_restrictfe_clearbesession tinyint(4) DEFAULT '0' NOT NULL,
 );
